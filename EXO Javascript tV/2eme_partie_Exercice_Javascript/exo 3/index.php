941f9f4cce609ac94e083a1e0f9bc281d0e10e7d
Thibault02 <118283087+Thibault02@users.noreply.github.com>
2023-10-10 15:16:08 +0200

    Add files via upload

---
+++ EXO Javascript tV/2eme_partie_Exercice_Javascript/exo 3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <img src="img/image1.jpg" class="testImage" onmouseover="this.src='img/image2.jpg'" onmouseout="this.src='img/image1.jpg'">
+    <img src="img/image3.jpg" class="testImage" onmouseover="this.src='img/image4.jpg'" onmouseout="this.src='img/image3.jpg'">
+    <img src="img/image5.jpeg" class="testImage" onmouseover="this.src='img/image6.jpeg'" onmouseout="this.src='img/image5.jpeg'">
+    <img src="img/image7.jpeg" class="testImage" onmouseover="this.src='img/image8.jpeg'" onmouseout="this.src='img/image7.jpeg'">
+    <img src="img/image9.jpeg" class="testImage" onmouseover="this.src='img/image10.jpeg'" onmouseout="this.src='img/image9.jpeg'">
+</body>
+</html>
---
+++ EXO Javascript tV/2eme_partie_Exercice_Javascript/exo 5/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p id="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus porro corporis illum voluptatum non eius magni recusandae. Eaque quo repellat assumenda saepe sequi nobis, veniam amet. Sapiente deserunt obcaecati vero!
+    Vel magni similique voluptatem quae placeat officiis est, vitae hic explicabo repellat odio, nobis rerum reprehenderit fuga quis distinctio quos? Quod enim aut molestiae error! Velit adipisci ipsum nam excepturi?
+    Odit nam maxime vitae quis. Labore ratione assumenda nisi, facilis consequatur, odit enim quisquam modi eum repellat id necessitatibus inventore. Dolore a iure odio sequi corrupti illum fugiat nulla incidunt?</p>
+    <button id="button" onclick="Afficher()">Afficher le texte</button>
+    <button id="button2" onclick="cacher()">Cacher le texte</button>
+    <button id="button3" onclick ="couleur()">Changer la couleur du texte</button>
+    <button id="button4" onclick ="resetCouleur()">Rénitialiser la couleur du text</button>
+    <script src="script.js"></script>
+</body>
+</html>
