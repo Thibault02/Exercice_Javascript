daea944b270bb1e424adc3a2004a03d7bf491d22
Thibault02 <118283087+Thibault02@users.noreply.github.com>
2023-10-12 10:08:10 +0200

    Add files via upload

---
+++ EXO Javascript tV/4eme_partie_Exercice_Javascript/Exo 2/script.js
@@ -0,0 +1,3 @@
+document.addEventListener('keydown', function(event) {
+    console.log("Touche pressée:", event.key);
+});
---
+++ EXO Javascript tV/4eme_partie_Exercice_Javascript/Exo 3/script.js
@@ -0,0 +1,3 @@
+document.getElementById("idButton").addEventListener('click',function(e){
+alert("l'action effectuer est : " +e.type)
+})
